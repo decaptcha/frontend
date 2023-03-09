b0d5313b994ed52fbfb008c5ff85de046bcf5df5
Archish <archishthakkar@gmail.com>
2023-03-09 21:12:29 +0530

    :art: fixed of wallet button

--- src/pages/researcher.tsx
+++ src/pages/researcher.tsx
@@ -28,7 +28,7 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Input,
-  Text
+  Text,
 } from "@chakra-ui/react";
 
 import { FaPlus } from "react-icons/fa";
@@ -188,150 +188,6 @@ const researcher = () => {
                 </Tfoot>
               </Table>
             </TableContainer>
-            {/* <Box pt="4">
-          <Card>
-            <CardHeader>
-              <Flex width="full" align="center" justify="space-between">
-                <CardTitle fontSize="xl">List of Projects</CardTitle>
-                <Button variant="primary" onClick={() => disclosure.onOpen()}>
-                  {" "}
-                  Create New Project{" "}
-                </Button>
-                <FormDialog
-                  title="Create New Project"
-                  defaultValues={{ title: "" }}
-                  {...disclosure}
-                  onSubmit={onSubmit}
-                >
-                  <FormLayout>
-                    <Field
-                      name="name"
-                      label="Name"
-                      type="text"
-                      rules={{ required: "Name is required" }}
-                      autoFocus
-                    />
-                    <Field
-                      name="corpusSize"
-                      label="Corpus Size"
-                      type="number"
-                      rules={{ required: "Corpus Size is required" }}
-                    />
-                    <Field
-                      name="labelName"
-                      label="Label Name"
-                      type="text"
-                      rules={{ required: "Label Name is required" }}
-                    />
-
-                    <Field
-                      name="threshold"
-                      label="Threshold"
-                      type="number"
-                      rules={{ required: "Label Name is required" }}
-                    />
-                    <Field
-                      name="duration"
-                      label="Expiry in Days"
-                      type="number"
-                      rules={{ required: "Expiry is required" }}
-                    />
-                    <Field
-                      name="description"
-                      type="textarea"
-                      label="Description"
-                    />
-                  </FormLayout>
-                </FormDialog>
-              </Flex>
-            </CardHeader>
-            <CardBody>
-              <List
-                items={[
-                  {
-                    onClick: () => datasetDisclosure.onOpen(),
-                    href: "#",
-                    primary: "Research dataset of Mercedes",
-                    secondary: "Cars",
-                    tertiary: <Tag color="green.300">Active</Tag>,
-                    action: (
-                      <Icon
-                        as={FiArrowRight}
-                        sx={{
-                          transitionProperty: "common",
-                          transitionDuration: "normal",
-                          ".chakra-button:hover &": {
-                            transform: "translate(5px)",
-                          },
-                        }}
-                      />
-                    ),
-                  },
-                  {
-                    href: "#",
-                    primary: "Dataset of ChatGPT",
-                    secondary: "Bus",
-                    tertiary: <Tag color="green.300">Active</Tag>,
-                    action: (
-                      <Icon
-                        as={FiArrowRight}
-                        sx={{
-                          transitionProperty: "common",
-                          transitionDuration: "normal",
-                          ".chakra-button:hover &": {
-                            transform: "translate(5px)",
-                          },
-                        }}
-                      />
-                    ),
-                  },
-                ]}
-              />
-            </CardBody>
-          </Card>
-          <FormDialog
-            title="Submit your dataset"
-            defaultValues={{ title: "" }}
-            {...datasetDisclosure}
-            onSubmit={onSubmit}
-          >
-            <FormLayout>
-              <SimpleGrid columns={2} gap="2">
-                <Card>
-                  <CardHeader>
-                    <CardTitle fontSize="xl">Labeled Dataset</CardTitle>
-                  </CardHeader>
-                  <CardBody>
-                    <Text fontSize="md">Upload your labeled dataset.</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <Button variant="solid" colorScheme="gray">
-                      Upload
-                    </Button>
-                  </CardFooter>
-                </Card>
-                <Card>
-                  <CardHeader>
-                    <CardTitle fontSize="xl">Unlabeled Dataset</CardTitle>
-                  </CardHeader>
-                  <CardBody>
-                    <Text fontSize="md">Upload your unlabeled dataset.</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <Button variant="solid" colorScheme="gray">
-                      Upload
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </SimpleGrid>
-            </FormLayout>
-          </FormDialog>
-          <Stat>
-            <StatLabel>Collected Fees</StatLabel>
-            <StatNumber>£0.00</StatNumber>
-            <StatHelpText>Feb 12 - Feb 28</StatHelpText>
-          </Stat>
-        </Box> */}
           </Flex>
         </Stack>
         <Drawer
@@ -400,34 +256,32 @@ const researcher = () => {
             <DrawerHeader>Create new project</DrawerHeader>
 
             <DrawerBody>
-              <SimpleGrid columns={2} gap="2">
-                <Card>
-                  <CardHeader>
-                    <Heading fontSize="xl">Labeled Dataset</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text>Upload your labeled dataset.</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <Button variant="solid" colorScheme="gray">
-                      Upload
-                    </Button>
-                  </CardFooter>
-                </Card>
-                <Card>
-                  <CardHeader>
-                    <Heading fontSize="xl">Unlabeled Dataset</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text>Upload your unlabeled dataset.</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <Button variant="solid" colorScheme="gray">
-                      Upload
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </SimpleGrid>
+              <Card>
+                <CardHeader>
+                  <Heading fontSize="xl">Labeled Dataset</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>Upload your labeled dataset.</Text>
+                </CardBody>
+                <CardFooter>
+                  <Button variant="solid" colorScheme="gray">
+                    Upload
+                  </Button>
+                </CardFooter>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <Heading fontSize="xl">Unlabeled Dataset</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>Upload your unlabeled dataset.</Text>
+                </CardBody>
+                <CardFooter>
+                  <Button variant="solid" colorScheme="gray">
+                    Upload
+                  </Button>
+                </CardFooter>
+              </Card>
             </DrawerBody>
 
             <DrawerFooter>

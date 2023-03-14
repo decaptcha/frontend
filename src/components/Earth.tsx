1eb035904382d0da9204c1898bf71ca610dd0494
Aditya Samantaray <aditya.sam@browserstack.com>
2023-03-15 02:35:50 +0530

    chore: add earth component

---
+++ src/components/Earth.tsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import * as THREE from "three";
+
+import EarthDayMap from "../../public/assets/8k_earth_daymap.jpg";
+import EarthNormalMap from "../../public/assets/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../public/assets/8k_earth_specular_map.jpg";
+import EarthCloudsMap from "../../public/assets/8k_earth_clouds.jpg";
+import { TextureLoader } from "three";
+
+export const Earth = () => {
+  // @ts-ignore
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+  // @ts-ignore
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
+  const earthRef: any = useRef();
+  const cloudsRef: any = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    earthRef.current.rotation.y = elapsedTime / 6;
+    cloudsRef.current.rotation.y = elapsedTime / 6;
+  });
+
+  return (
+    <>
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={20000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
+      <mesh ref={cloudsRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
+        {/* <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        /> */}
+      </mesh>
+    </>
+  );
+}

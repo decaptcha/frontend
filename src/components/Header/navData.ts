6ed05016cfdde6d79f7a571424d275d3a063bc4d
Archish <archishthakkar@gmail.com>
2023-03-15 06:53:47 +0530

    :art: added twitter in nav bar

--- src/components/Header/navData.ts
+++ src/components/Header/navData.ts
@@ -17,6 +17,11 @@ export interface NavItem {
 
 export const NAV_ITEMS: Array<NavItem> = [
   {
+    label: "Twitter",
+    href: `https://twitter.com/decaptchaio`,
+    passHref: false,
+  },
+  {
     label: "Demo Site",
     href: `http://localhost:8083`,
     passHref: false,
